66264088269ce4f5720213432f5148ccc0500bc9
bhattshrey24 <53301200+bhattshrey24@users.noreply.github.com>
2020-10-21 21:10:11 +0530

    Update UserInputForm.dart

--- bankingApp/lib/Screen/UserInputForm.dart
+++ bankingApp/lib/Screen/UserInputForm.dart
@@ -12,28 +12,23 @@ class UserInputForm extends StatefulWidget {
 class _UserInputFormState extends State<UserInputForm> {
   final _userInputForm = GlobalKey<FormState>();
   User user = User(
-      // maybe initializing id can create problem
+     
       0,
       '',
       '',
       0);
   void _saveForm() {
-    bool validate = _userInputForm.currentState.validate();
-    if (!validate) {
-      return;
-    }
+  
     _userInputForm.currentState.save();
-    // print('$_storedImage is the value of stored image');
+   
     Provider.of<UserProvider>(context, listen: false).addUser(user).then(
       (value) {
         print('add student complete inside its then function');
-        //Navigator.pop(context, _editedDetails.name);
+       
         Navigator.pop(context);
       },
     );
 
-    // FocusScope.of(context).unfocus();
-    // Navigator.of(context).pop();
   }
 
   @override
