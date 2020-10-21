2df6aa921874375d94f39cb8cf82519f7ae5a5bd
bhattshrey24 <53301200+bhattshrey24@users.noreply.github.com>
2020-10-21 21:11:55 +0530

    Update dbHelper.dart

--- bankingApp/lib/helper/dbHelper.dart
+++ bankingApp/lib/helper/dbHelper.dart
@@ -13,11 +13,10 @@ class AppDB {
     }
     _db = await AppDB._initDB();
     return _db;
-  } // remember you cant have a coulumn name with space eg 'student name'is wrong instead have a name 'student_name'
-
+  }
   static _initDB() async {
     io.Directory documentsDirectory = await path2
-        .getApplicationDocumentsDirectory(); // THIS RETURNS PATH TO PLACE WHERE WE'LL STORE THE DB
+        .getApplicationDocumentsDirectory(); 
     var pathOfDB = path1.join(documentsDirectory.path, 'bankingDB4.db');
     var db =
         await sql.openDatabase(pathOfDB, version: 1, onCreate: AppDB._createDB);
@@ -25,29 +24,29 @@ class AppDB {
   }
 
   static _createDB(sql.Database db, int newVersion) async {
-    //these three qoutes ''' enables us to write String like the way it is written below , now even after beautificaion it wont realign in a single line
+   
     await db.execute(
-        'CREATE TABLE User_Details(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,email TEXT,credit INT)'); //Maybe i had to change data type of credit field
+        'CREATE TABLE User_Details(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,email TEXT,credit INT)'); 
     await db.execute(
         'CREATE TABLE Transfer_Table(txnId INTEGER PRIMARY KEY AUTOINCREMENT,transferFrom TEXT,transferTo TEXT,transferAmount INT)');
     print('inside createDB');
   }
 
   static Future<int> insert(String tableName, Map<String, Object> data) async {
-    // this method inserts the data in database
+   
     print('inside insert');
     final db = await AppDB.database();
 
     return db.insert(tableName, data,
         conflictAlgorithm: sql.ConflictAlgorithm
-            .replace); // conflic argument is used cause if we already have a entery with an id then it overwrites the data with new data
+            .replace); 
   }
 
   static Future<List<Map<String, dynamic>>> getData(String tableName) async {
     print('inside getdata');
-    //this method is to fetch/get data from database
+   
     final db = await AppDB
-        .database(); // we have to explicitly do classname.function_name cause it is static method and if we dont do this it will find this function as global method
+        .database(); 
     return db.query(tableName);
   }
 }
